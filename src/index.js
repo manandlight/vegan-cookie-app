4703554b84311c027d42f1b9c00d4d23e31deca1
manandlight <ishib@proton.me>
2025-03-02 15:57:36 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';  // この行を追加
+import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
